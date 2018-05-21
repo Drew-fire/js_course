4ab16a4a8deb2c9938c0d27c2c93312b09a8c2c9
Drew-fire <abrosenkov91@gmail.com>
2018-05-22 00:01:44 +0300

    lesson_15_no_comment

--- lesson_15/index.js
+++ lesson_15/index.js
@@ -1,33 +1,3 @@
-// function init ( arrObj, n ) {
-
-// 	let li = document.body.appendChild(document.createElement('ul'));
-
-// 	for (i = 0; i < n; i++) {
-
-// 		let li = ul.appendChild(document.createElement('li'));
-
-// 		li.className = arrObj.className;
-
-// 		for (key in arrObj.attributes) {
-// 			li.setAttribute(key, arrObj.attributes[key])
-// 		}
-// 		li.innerHTML = arrObj.content;
-// 	}
-// }
-
-// init ({ className: 'class', attributes: { 'data-name': 'Anatoliy','data-name2': 'Anatoliy2' }, content: 'Some Text' }, 5);
-
-// function listen() {
-// 	let ul = document.querySelector('ul');
-// 	if (ul) {
-// 		document.body.removeChild(ul);
-// 		return console.log(true);
-// 	} else {
-// 		return console.log(false);
-// 	}
-// }
-
-// document.querySelector('button').addEventListener('click', listen);
 
 function init ( arrObj, n ) {
 
