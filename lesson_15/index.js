752e45fe59f1cf659e64c4c3281faae661f42ab7
Drew-fire <abrosenkov91@gmail.com>
2018-05-25 01:42:44 +0300

    lesson_15_v_2

--- lesson_15/index.js
+++ lesson_15/index.js
@@ -1,43 +1,107 @@
 
-function init ( arrObj, n ) {
+function init( arr, n ) {
 
-	let ul = document.createElement('ul');
+	const ul = document.createElement('ul');
+  
+  for (let i = 0; i < arr.length; i += 1) {
+  	const el = arr[i];
+    
+    for (let j = 0; j < n; j += 1) {
 
-	document.body.appendChild(ul);
+    		const li = document.createElement('li');
 
-	for (i = 0; i < n; i++) {
+        li.className = el.className;
+        
+/*         for (const key in el.attributes) {
+          li.setAttribute(key, el.attributes[key]);
+        } */
+        
+        const keys = Object.keys(el.attributes);
+        
+        for (let l = 0; l < keys.length; l += 1) {
 
-		let li = document.createElement('li');
+        	const key = keys[l];
 
-		ul.appendChild(li);
-
-		li.className = arrObj.className;
-
-		for (key in arrObj.attributes) {
-
-			li.setAttribute(key, arrObj.attributes[key])
-		}
-		li.innerHTML = arrObj.content;
-	}
+          li.setAttribute(key, el.attributes[key]);
+        }
+        
+        li.appendChild(document.createTextNode(el.content));
+        
+        ul.appendChild(li);
+    }
+  }
+  
+  document.body.appendChild(ul);
 }
+
+/* init([
+  { className: 'a1', attributes: { 'data-name': 'MyName', 'data-age': '43' }, content: '123' },
+  { className: 'a2', attributes: {}, content: '122456' }
+], 2); */
 
 function listen() {
 
-	const btn = document.getElementsByTagName("button");
+	const b = document.getElementsByTagName('button')[0];
+  
+  b.addEventListener('click', function () {
 
-	btn.addEventListener('click', function() {
-
-		let ul = document.querySelector('ul');
-
-		if (ul) {
-
-			document.body.removeChild(ul);
-
-			return true;
-
-		} else {
-
-			return false;
-		}
-	});
+  	const ul = document.querySelector('ul');
+    
+    if (ul) {
+    	
+    	document.body.removeChild(ul);
+      
+      return true;
+    }
+    
+    return false;
+  });
 }
+
+/* listen(); */
+
+
+
+
+// function init ( arrObj, n ) {
+
+// 	let ul = document.createElement('ul');
+
+// 	document.body.appendChild(ul);
+
+// 	for (i = 0; i < n; i++) {
+
+// 		let li = document.createElement('li');
+
+// 		ul.appendChild(li);
+
+// 		li.className = arrObj.className;
+
+// 		for (key in arrObj.attributes) {
+
+// 			li.setAttribute(key, arrObj.attributes[key])
+// 		}
+// 		li.innerHTML = arrObj.content;
+// 	}
+// }
+
+// function listen() {
+
+// 	const btn = document.getElementsByTagName("button");
+
+// 	btn.addEventListener('click', function() {
+
+// 		let ul = document.querySelector('ul');
+
+// 		if (ul) {
+
+// 			document.body.removeChild(ul);
+
+// 			return true;
+
+// 		} else {
+
+// 			return false;
+// 		}
+// 	});
+// }
