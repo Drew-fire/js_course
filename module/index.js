e2b746b94e0bb178f6a66626d324109e8f8fe9df
Drew-fire <abrosenkov91@gmail.com>
2018-05-25 21:42:01 +0300

    function_5

--- module/index.js
+++ module/index.js
@@ -50,4 +50,61 @@ function row (a, b, c, str ) {
 
 }
 
-// ===========================================___Чеивёртое задание___======================================= 
+// ===========================================___Четвёртое задание___======================================= 
+
+function fact (n) {
+	let result;
+	if (n == 0) {
+		result = 1;
+	}
+
+	else {result = (n != 1) ? n * fact(n - 1) : 1;
+
+	}
+
+	return result;
+		
+}
+
+// ===========================================___Пятое задание___======================================= 
+
+function matrixDiff (arr1, arr2) {
+
+	 let sumArr1 = 0;
+	 let sumArr2 = 0;
+	 let result = 0;
+	 let sum2 = 0;
+	 let resultArr = [];
+
+ for (let x = 0; x < arr1.length; x += 1 ) {
+
+	   for(let y = 0; y < arr1[x].length; y += 1 ) {
+
+		    if (arr1[x].length != arr2[x].length)
+
+		     return "NaN"
+
+		    sumArr1 = arr1[x][y];
+
+		    sumArr2 =arr2[x][y];
+
+		    resultArr[sum2] = Math.abs(sumArr1 - sumArr2);
+
+		    sum2 += 1;
+
+	   }
+
+	 }
+
+	 for (let i = 0; i < resultArr.length; i += 1) {
+
+	  result += resultArr[i];
+
+	 }
+
+return result;
+
+}
+
+
+// ===========================================___Шестое задание___======================================= 
