44d8a1e9ea30ce4d663c5e84dcd629245c1cf75d
Drew-fire <abrosenkov91@gmail.com>
2018-06-08 00:17:57 +0300

    lesson_20 old sintax

--- lesson_20/index.js
+++ lesson_20/index.js
@@ -1,6 +1,6 @@
 
 
-////////////////////////////////____new sintax____////////////////////////////////
+////////////////////////////////____New sintax____////////////////////////////////
 
 class Human {
 	constructor(obj) {
@@ -47,3 +47,34 @@ class AlevelStudent extends Human {
 // console.log(student);
 
 ///////////////////////////////////
+
+
+//////////////////////////////////____Old sintax____////////////////////////////////
+
+// function Human(obj) {
+// 	this.name = obj.name;
+// 	this.age = obj.age;
+// }
+
+// Human.prototype.sayHello = function() {
+// 	console.log( `Hello, my name is  ${this.name}, i am ${this.age} years old` );
+// }
+
+// function AlevelStudent(obj) {
+// 	this.marks = obj.marks;
+// }
+
+// AlevelStudent.prototype.averageMark = function() {
+// 	let sum = 0;
+// 		let average = 0;
+// 		for (let j in this.marks) {
+// 				sum += this.marks[j];
+// 		}
+
+// 		average = sum / this.marks.length;
+
+// 		console.log( `My average score is: ${average}.` );
+		
+// }
+
+///////////////////////////////////////////////////////////////////////////////
